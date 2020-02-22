32f73dd78fb944b0512123435b99015b8bc731f0
Christine Rehm <rehm.christine@gmail.com>
2020-02-21 20:48:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/flashcardfrenzy/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.flashcardfrenzy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setBackgroundColor(getResources().getColor(R.color.questionBackground,null));
+
+        findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.answerBackground, null));
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+}
